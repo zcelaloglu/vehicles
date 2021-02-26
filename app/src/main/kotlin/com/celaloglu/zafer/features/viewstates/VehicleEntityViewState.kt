729598571b9dcf037d65799c780874340649a6e0
TURKCELL\TCZCELALOGLU <zafer.celaloglu@turkcell.com.tr>
2021-02-26 08:33:27 +0300

    move data source business logic from xml layout to view state to be able to test it

--- app/src/main/kotlin/com/celaloglu/zafer/features/viewstates/VehicleEntityViewState.kt
+++ app/src/main/kotlin/com/celaloglu/zafer/features/viewstates/VehicleEntityViewState.kt
@@ -8,4 +8,54 @@ class VehicleEntityViewState(private val vehicle: Vehicle) {
     fun getTitle() = vehicle.title
     fun getPrice() = vehicle.price
     fun getFuelType() = vehicle.fuelType
+
+    private fun isImageNotNull() = getImageUrls().isNullOrEmpty().not()
+
+    fun isFirstImageVisible() = isImageNotNull()
+    fun isSecondImageVisible() = isImageNotNull() && getImageUrls()?.size!! >= 2
+    fun isThirdImageVisible() = isImageNotNull() && getImageUrls()?.size!! >= 3
+    fun isFourthImageVisible() = isImageNotNull() && getImageUrls()?.size!! >= 4
+
+    fun isFifthImageVisible() = isImageNotNull() && getImageUrls()?.size!! == 5
+
+    fun firstImageUrl(): String {
+        return if (isImageNotNull()) {
+            vehicle.imageUrls?.get(0)!!
+        } else {
+            ""
+        }
+    }
+
+    fun secondImageUrl(): String {
+        return if (vehicle.imageUrls != null && vehicle.imageUrls.size >= 2) {
+            vehicle.imageUrls[1]
+        } else {
+            ""
+        }
+    }
+
+    fun thirdImageUrl(): String {
+        return if (vehicle.imageUrls != null && vehicle.imageUrls.size >= 3) {
+            vehicle.imageUrls[2]
+        } else {
+            ""
+        }
+    }
+
+    fun fourthImageUrl(): String {
+        return if (vehicle.imageUrls != null && vehicle.imageUrls.size >= 4) {
+            vehicle.imageUrls[3]
+        } else {
+            ""
+        }
+    }
+
+    fun fifthImageUrl(): String {
+        return if (vehicle.imageUrls != null && vehicle.imageUrls.size == 5) {
+            vehicle.imageUrls[4]
+        } else {
+            ""
+        }
+    }
+
 }
--- app/src/test/kotlin/com/celaloglu/zafer/viewstate/VehicleEntityViewStateTest.kt
+++ app/src/test/kotlin/com/celaloglu/zafer/viewstate/VehicleEntityViewStateTest.kt
@@ -7,11 +7,24 @@ import org.junit.Test
 
 class VehicleEntityViewStateTest {
 
+    private val vehicle by lazy {
+        Vehicle(
+                id = 1,
+                imageUrls = listOf(
+                        "https://loremflickr.com/g/320/240/bmw",
+                        "https://loremflickr.com/g/640/480/bmw",
+                        "https://loremflickr.com/g/1600/1200/bmw"
+                ),
+                title = "BMW 316i",
+                price = 13000,
+                fuelType = "Gasoline"
+        )
+    }
+
     @Test
     fun `should match title for given vehicle`() {
 
         // Given
-        val vehicle = createDummyVehicle()
         val givenViewState = VehicleEntityViewState(vehicle)
 
         // When
@@ -25,7 +38,6 @@ class VehicleEntityViewStateTest {
     fun `should match image urls for given vehicle`() {
 
         // Given
-        val vehicle = createDummyVehicle()
         val givenViewState = VehicleEntityViewState(vehicle)
 
         // When
@@ -45,7 +57,6 @@ class VehicleEntityViewStateTest {
     fun `should match fuel type for given vehicle`() {
 
         // Given
-        val vehicle = createDummyVehicle()
         val givenViewState = VehicleEntityViewState(vehicle)
 
         // When
@@ -59,7 +70,6 @@ class VehicleEntityViewStateTest {
     fun `should match price for given vehicle`() {
 
         // Given
-        val vehicle = createDummyVehicle()
         val givenViewState = VehicleEntityViewState(vehicle)
 
         // When
@@ -69,17 +79,134 @@ class VehicleEntityViewStateTest {
         Truth.assertThat(actualResult).isEqualTo(13000)
     }
 
-    private fun createDummyVehicle(): Vehicle {
-        return Vehicle(
-            id = 1,
-            imageUrls = listOf(
-                "https://loremflickr.com/g/320/240/bmw",
-                "https://loremflickr.com/g/640/480/bmw",
-                "https://loremflickr.com/g/1600/1200/bmw"
-            ),
-            title = "BMW 316i",
-            price = 13000,
-            fuelType = "Gasoline"
-        )
+    @Test
+    fun `should match first image url for given vehicle`() {
+
+        // Given
+        val givenViewState = VehicleEntityViewState(vehicle)
+
+        // When
+        val actualResult = givenViewState.firstImageUrl()
+
+        // Then
+        Truth.assertThat(actualResult).isEqualTo("https://loremflickr.com/g/320/240/bmw")
     }
+
+    @Test
+    fun `should match second image url for given vehicle`() {
+
+        // Given
+        val givenViewState = VehicleEntityViewState(vehicle)
+
+        // When
+        val actualResult = givenViewState.secondImageUrl()
+
+        // Then
+        Truth.assertThat(actualResult).isEqualTo("https://loremflickr.com/g/640/480/bmw")
+    }
+
+    @Test
+    fun `should match third image url for given vehicle`() {
+
+        // Given
+        val givenViewState = VehicleEntityViewState(vehicle)
+
+        // When
+        val actualResult = givenViewState.thirdImageUrl()
+
+        // Then
+        Truth.assertThat(actualResult).isEqualTo("https://loremflickr.com/g/1600/1200/bmw")
+    }
+
+    @Test
+    fun `should match fourth image url for given vehicle`() {
+
+        // Given
+        val givenViewState = VehicleEntityViewState(vehicle)
+
+        // When
+        val actualResult = givenViewState.fourthImageUrl()
+
+        // Then
+        Truth.assertThat(actualResult).isEqualTo("")
+    }
+
+    @Test
+    fun `should match fifth image url for given vehicle`() {
+
+        // Given
+        val givenViewState = VehicleEntityViewState(vehicle)
+
+        // When
+        val actualResult = givenViewState.fifthImageUrl()
+
+        // Then
+        Truth.assertThat(actualResult).isEqualTo("")
+    }
+
+    @Test
+    fun `should match first image visibility for given vehicle`() {
+
+        // Given
+        val givenViewState = VehicleEntityViewState(vehicle)
+
+        // When
+        val actualResult = givenViewState.isFirstImageVisible()
+
+        // Then
+        Truth.assertThat(actualResult).isEqualTo(true)
+    }
+
+    @Test
+    fun `should match second image visibility for given vehicle`() {
+
+        // Given
+        val givenViewState = VehicleEntityViewState(vehicle)
+
+        // When
+        val actualResult = givenViewState.isSecondImageVisible()
+
+        // Then
+        Truth.assertThat(actualResult).isEqualTo(true)
+    }
+
+    @Test
+    fun `should match third image visibility for given vehicle`() {
+
+        // Given
+        val givenViewState = VehicleEntityViewState(vehicle)
+
+        // When
+        val actualResult = givenViewState.isThirdImageVisible()
+
+        // Then
+        Truth.assertThat(actualResult).isEqualTo(true)
+    }
+
+    @Test
+    fun `should match fourth image visibility for given vehicle`() {
+
+        // Given
+        val givenViewState = VehicleEntityViewState(vehicle)
+
+        // When
+        val actualResult = givenViewState.isFourthImageVisible()
+
+        // Then
+        Truth.assertThat(actualResult).isEqualTo(false)
+    }
+
+    @Test
+    fun `should match fifth image visibility for given vehicle`() {
+
+        // Given
+        val givenViewState = VehicleEntityViewState(vehicle)
+
+        // When
+        val actualResult = givenViewState.isFifthImageVisible()
+
+        // Then
+        Truth.assertThat(actualResult).isEqualTo(false)
+    }
+
 }
